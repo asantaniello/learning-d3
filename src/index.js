188ccc4420413e706743782778233c8fbe70163b
Adam Santaniello <adam.santaniello@ucsf.edu>
2017-06-19 01:28:57 -0700

    Incorporates ch4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/*
 // Chapter 2 stuff:
 var ch2 = require('./chapter2') ;
 ch2.default() ;
@@ -5,12 +6,24 @@ ch2.axisDemo() ;
 ch2.FunkyD3PathGenerators() ;
 ch2.myWeirdSVGDrawing() ;
 ch2.renderDailyShowGuestTable() ;
+*/
 
-
+/*
 // Chapter 3 stuff:
 import 'babel-core/polyfill';
 import {UlamSpiral, ScalesDemo, GeoDemo} from './chapter3';
 new UlamSpiral();
 new ScalesDemo();
 new GeoDemo();
+*/
 
+/*
+// Chapter 4 stuff:
+import {Easingchart, Spirograph, PrisonPopulationChart, InteractivePrisonPopulationChart, DraggableInteractivePrisonChart, SelectableInteractivePrisonChart} from './chapter4';
+//new Easingchart() ;
+//new Spirograph() ;
+//new PrisonPopulationChart('data/uk_prison_data_1900-2015.csv');
+//new InteractivePrisonPopulationChart('data/uk_prison_data_1900-2015.csv');
+//new DraggableInteractivePrisonChart('data/uk_prison_data_1900-2015.csv');
+new SelectableInteractivePrisonChart('data/uk_prison_data_1900-2015.csv');
+    */
